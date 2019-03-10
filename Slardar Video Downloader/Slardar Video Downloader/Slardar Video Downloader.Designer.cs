f87a6d0108a0d03b5ec29face72e36cfd5abd968
Slardar <me25show@gmail.com>
2019-03-11 09:35:49 +1100

    Set browser

---
+++ Slardar Video Downloader/Slardar Video Downloader/Slardar Video Downloader.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Slardar_Video_Downloader
+{
+    partial class Slardar_Video_Downloader
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.muteCheckBox = new System.Windows.Forms.CheckBox();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.mainLayoutPanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.muteCheckBox);
+            this.panel1.Controls.Add(this.urlTextBox);
+            this.panel1.Controls.Add(this.urlLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 52);
+            this.panel1.TabIndex = 6;
+            // 
+            // muteCheckBox
+            // 
+            this.muteCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.muteCheckBox.AutoSize = true;
+            this.muteCheckBox.Checked = true;
+            this.muteCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.muteCheckBox.Location = new System.Drawing.Point(930, 14);
+            this.muteCheckBox.Name = "muteCheckBox";
+            this.muteCheckBox.Size = new System.Drawing.Size(70, 25);
+            this.muteCheckBox.TabIndex = 8;
+            this.muteCheckBox.Text = "Mute";
+            this.muteCheckBox.UseVisualStyleBackColor = true;
+            this.muteCheckBox.CheckedChanged += new System.EventHandler(this.muteCheckBox_CheckedChanged);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlTextBox.Location = new System.Drawing.Point(61, 12);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(863, 29);
+            this.urlTextBox.TabIndex = 7;
+            this.urlTextBox.Text = "https://www.slardar.net";
+            this.urlTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.urlTextBox_KeyPressed);
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.urlLabel.Location = new System.Drawing.Point(9, 15);
+            this.urlLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(44, 21);
+            this.urlLabel.TabIndex = 6;
+            this.urlLabel.Text = "URL:";
+            // 
+            // mainLayoutPanel
+            // 
+            this.mainLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainLayoutPanel.Location = new System.Drawing.Point(0, 52);
+            this.mainLayoutPanel.Name = "mainLayoutPanel";
+            this.mainLayoutPanel.Size = new System.Drawing.Size(1008, 677);
+            this.mainLayoutPanel.TabIndex = 7;
+            // 
+            // Slardar_Video_Downloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.mainLayoutPanel);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Slardar_Video_Downloader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slardar Video Downloader";
+            this.Load += new System.EventHandler(this.Slardar_Video_Downloader_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox muteCheckBox;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.Panel mainLayoutPanel;
+    }
+}
+
---
+++ Slardar Video Downloader/Slardar Video Downloader/Slardar Video Downloader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNetBrowser;
+using DotNetBrowser.WinForms;
+
+namespace Slardar_Video_Downloader
+{
+    public partial class Slardar_Video_Downloader : Form
+    {
+        private WinFormsBrowserView browserView;
+        public Slardar_Video_Downloader()
+        {
+            InitializeComponent();
+        }
+
+        private void Slardar_Video_Downloader_Load(object sender, EventArgs e)
+        {
+            browserView = new WinFormsBrowserView();
+            mainLayoutPanel.Controls.Add((Control)browserView);
+            browserView.Dock = DockStyle.Fill;
+            browserView.Browser.AudioMuted = true;
+            browserView.Browser.LoadURL(urlTextBox.Text);
+        }
+
+        private void urlTextBox_KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                browserView.Browser.LoadURL(urlTextBox.Text);
+            }
+        }
+
+        private void muteCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            browserView.Browser.AudioMuted = muteCheckBox.Checked;
+        }
+    }
+}
